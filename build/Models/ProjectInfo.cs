1334c0a1a5594e50b95ba1b507aa67cdd7bc3a2d
Tom Wolfe <tom.wolfe@cet-uk.com>
2025-08-12 11:06:49 +0100

    build: remove build

--- build/Models/ProjectInfo.cs
+++
@@ -1,9 +0,0 @@
-using NuGet.Versioning;
-
-namespace Hamelin.Runtimes.GitHubActions.Build.Models;
-
-public class ProjectInfo
-{
-    public required string Name { get; set; }
-    public required NuGetVersion Version { get; init; }
-}
--- build/Services/ICommandRunner.cs
+++
@@ -1,6 +0,0 @@
-namespace Hamelin.Runtimes.GitHubActions.Build.Services;
-
-public interface ICommandRunner
-{
-    Task Run(string command, string[] arguments, CancellationToken cancellationToken);
-}
