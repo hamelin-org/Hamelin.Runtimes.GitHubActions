9750729a66e6cb8bdfd5f4ac70a08a0ea4944139
Tom Wolfe <tom.wolfe@cet-uk.com>
2025-08-14 12:51:51 +0100

    chore: formatting

--- src/Hamelin.Runtimes.GitHubActions/GitHubActionsCommandsStub.cs
+++ src/Hamelin.Runtimes.GitHubActions/GitHubActionsCommandsStub.cs
@@ -5,13 +5,16 @@ internal class GitHubActionsCommandsStub : IGitHubActionsCommands
     public void LogDebug(string message) { }
 
     public void LogNotice(string message, string? title = null, string? file = null, int? startLine = null,
-        int? endLine = null, int? startColumn = null, int? endColumn = null) { }
+        int? endLine = null, int? startColumn = null, int? endColumn = null)
+    { }
 
     public void LogWarning(string message, string? title = null, string? file = null, int? startLine = null,
-        int? endLine = null, int? startColumn = null, int? endColumn = null) { }
+        int? endLine = null, int? startColumn = null, int? endColumn = null)
+    { }
 
     public void LogError(string message, string? title = null, string? file = null, int? startLine = null,
-        int? endLine = null, int? startColumn = null, int? endColumn = null) { }
+        int? endLine = null, int? startColumn = null, int? endColumn = null)
+    { }
 
     public void BeginGroup(string title) { }
 
